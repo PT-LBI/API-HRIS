3eda6db3a9903851992e95ea34947a55242e9a74
didit <diditjamianto7@gmail.com>
2024-09-11 20:24:48 +0700

    update

--- app/Http/Controllers/Mobile/MyLeaveController.php
+++ app/Http/Controllers/Mobile/MyLeaveController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\mobile;
+namespace App\Http\Controllers\Mobile;
 
 use App\Http\Controllers\Controller;
 use App\Models\Leave;
--- app/Http/Controllers/Mobile/MyNotifController.php
+++ app/Http/Controllers/Mobile/MyNotifController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\mobile;
+namespace App\Http\Controllers\Mobile;
 
 use App\Models\LogNotif;
 
